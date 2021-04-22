c28bfd6886f3f15c06e40c3265c1e7dd8b9fceb5
linsheng24 <linsheng24@gmail.com>
2021-04-23 01:40:19 +0800

    修改 redis adapter 的版本、修改 appGatway.ts

--- src/appGateway.ts
+++ src/appGateway.ts
@@ -1,17 +1,38 @@
 import {
+  ConnectedSocket,
   MessageBody,
   SubscribeMessage,
   WebSocketGateway,
   WebSocketServer,
+  WsResponse,
 } from '@nestjs/websockets';
+import { Socket } from 'socket.io';
+import { from, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
-@WebSocketGateway()
+@WebSocketGateway({ namespace: 'chat' })
 export class AppGateway {
   @WebSocketServer()
   server;
 
-  @SubscribeMessage('events')
-  findAll(@MessageBody() data: any) {
-    console.log('accepted message');
+  handleConnection() {
+    console.log('new connection!');
+  }
+
+  @SubscribeMessage('join')
+  joinChannel(client: Socket, room: string) {
+    client.join(room);
+  }
+
+  @SubscribeMessage('leave')
+  leaveChannel(client: Socket, room: string) {
+    client.leave(room);
+  }
+
+  @SubscribeMessage('test')
+  testChannel(client: Socket, room: string) {
+    // 不包刮自己
+    // client.broadcast.to('test_room').emit('test');
+    this.server.to('test_room').emit('test');
   }
 }
